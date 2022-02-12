63ca9a8dd628bc85275c4ff0093abd62bf97b59d
arnavnarain <narain.a@northeastern.edu>
2022-02-12 10:15:00 -0500

    Search and Listview functioning.

--- app/src/main/java/com/discretesurvivors/physicscalculator/MainActivity.java
+++ app/src/main/java/com/discretesurvivors/physicscalculator/MainActivity.java
@@ -3,10 +3,17 @@ package com.discretesurvivors.physicscalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,28 +26,36 @@ public class MainActivity extends AppCompatActivity {
     k = 1/2 Iw^2
 
      */
+    private ListView formulasDisplay;
+    ArrayAdapter<String> formulasAdapter;
+    EditText inputSearch;
+    ArrayList<HashMap<String, String>> formulasList;
 
-    ArrayList<String> formulas = new ArrayList<String>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        formulas.add("velocity");
-        formulas.add("distance");
-        formulas.add("vx^2");
-        formulas.add("w");
-        formulas.add("k");
+        String formulas[] = {"velocity", "distance", "vx^2",
+                "w", "k"};
+        formulasDisplay = (ListView) findViewById(R.id.editlist_view);
+        inputSearch = (EditText) findViewById(R.id.itemSearch);
+        formulasAdapter = new ArrayAdapter<String>
+                (this, R.layout.searchview, R.id.product_name, formulas);
+        formulasDisplay.setAdapter(formulasAdapter);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+                MainActivity.this.formulasAdapter.getFilter().filter(cs);
+            }
 
-        LinearLayout LinearLayoutView = new LinearLayout(this);
-        TextView tv = (TextView) findViewById(R.id.textView_main);
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+            }
 
-        StringBuilder formulasBuilder = new StringBuilder();
+            public void afterTextChanged(Editable arg0) {
 
-        for(int i =0; i < formulas.size(); i++) {
-            formulasBuilder.append(formulas.get(i) + "\n");
-        }
-        tv.setText(formulasBuilder.toString());
+            }
+        });
 
     }
 }
